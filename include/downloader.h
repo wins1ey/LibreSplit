1ece75a3b447b35eaa3b0443aeec4ff0656dcaf4
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-11 23:16:22 +0100

    Created header file for downloader.cpp

---
+++ include/downloader.h
@@ -0,0 +1,21 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <curl/curl.h>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::istringstream;
+
+void startDownloader(string autoSplittersDirectory);
+void downloadFile(string url);
+
+#endif
