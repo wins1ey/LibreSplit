4bca354e777ad4c830f40a60fbe786ebc04a9b7e
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-11 17:41:32 +0100

    readMem throws exceptions instead of printing errors

--- readmem.hpp
+++ readmem.hpp
@@ -16,6 +16,7 @@
 using std::cout;
 using std::endl;
 using std::cin;
+using std::to_string;
 
 class ReadMemory
 {
@@ -35,13 +36,11 @@ class ReadMemory
             ssize_t memNread = process_vm_readv(pid, &memLocal, 1, &memRemote, 1, 0);
             if (memNread == -1)
             {
-                cout << "Error reading process memory: " << strerror(errno) << endl;
-                exit(-1);
+                throw std::runtime_error("Error reading process memory");
             }
             else if (memNread != memRemote.iov_len)
             {
-                cout << "Error reading process memory: short read of " << memNread << " bytes" << endl;
-                exit(-1);
+                throw std::runtime_error("Error reading process memory: short read of " + to_string(memNread) + " bytes\n");
             }
 
             return value;  // Return the read value
