634923a753e93afd503b6da675a67bbb15289f40
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-12 00:25:35 +0100

    Created header file for lasprint

---
+++ include/lasprint.h
@@ -0,0 +1,13 @@
+#ifndef LASPRINT_H
+#define LASPRINT_H
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void lasPrint(string message);
+
+#endif
