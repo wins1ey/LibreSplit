51da5241c71b4e924dde4872797d28618bff3866
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-11 14:09:25 +0100

    Declared memLocal and memRemote in class

--- readmem.hpp
+++ readmem.hpp
@@ -21,8 +21,11 @@ using std::cin;
 
 class ReadMemory
 {
+    struct iovec memLocal;
+    struct iovec memRemote;
+
     public:
-        uint8_t readMem8(uint32_t mem, int pid, uint64_t memAddress, struct iovec memLocal, struct iovec memRemote)
+        uint8_t readMem8(int pid, uint64_t memAddress)
         {
             uint32_t value;  // Variable to store the read value
 
@@ -46,7 +49,7 @@ class ReadMemory
             return value;  // Return the read value
         }
 
-        uint64_t readMem64(uint32_t mem, int pid, uint64_t memAddress, struct iovec memLocal, struct iovec memRemote)
+        uint64_t readMem64(int pid, uint64_t memAddress)
         {
             uint32_t value;  // Variable to store the read value
 
@@ -70,7 +73,7 @@ class ReadMemory
             return value;  // Return the read value
         }
 
-        uint32_t readMem32(uint32_t mem, int pid, uint64_t memAddress, struct iovec memLocal, struct iovec memRemote)
+        uint32_t readMem32(int pid, uint64_t memAddress)
         {
             uint32_t value;  // Variable to store the read value
 
