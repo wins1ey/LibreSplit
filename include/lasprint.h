3fd6f8a561e3dd41c8f556b650eb5ad9c98d8efa
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-12 09:36:19 +0100

    Added lua header

--- include/lasprint.h
+++ include/lasprint.h
@@ -3,11 +3,13 @@
 
 #include <iostream>
 #include <string>
+#include <lua.hpp>
 
 using std::string;
 using std::cout;
 using std::endl;
 
 void lasPrint(string message);
+int luaPrint(lua_State* L);
 
 #endif
