3598e00de7ebb0075155c82e323bc92660a98305
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-15 19:41:40 +0100

    Removed unused code

--- src/readmem.h
+++ src/readmem.h
@@ -1,14 +1,9 @@
 #ifndef READMEM_H
 #define READMEM_H
 
-#include <cstdint>
-
 #include <lua.hpp>
 
 int findProcessID(lua_State* L);
 int readAddress(lua_State* L);
-
-template <typename T>
-T readMemory(uintptr_t memAddress);
 
 #endif
