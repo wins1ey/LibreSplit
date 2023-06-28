b0f7709935b37b4eca37425e91e2c9e932134404
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-28 19:19:07 +0100

    Run timer and auto splitter at the same time

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <cstring>
+#include <thread>
 
 #include "headers/client.hpp"
 #include "headers/downloader.hpp"
 #include "headers/autosplitter.hpp"
+extern "C" {
+    #include "headers/urn-gtk.h"
+}
+
+using std::thread;
 
 void launchArgs(int argc, char *argv[])
 {
@@ -15,13 +21,31 @@ void launchArgs(int argc, char *argv[])
     }
 }
 
-int main(int argc, char *argv[])
+int autoSplitterThread(int argc, char *argv[])
 {
     checkDirectories();
     launchArgs(argc, argv);
     chooseAutoSplitter();
     setIpAddress();
     runAutoSplitter();
+
+    return 0;
+}
+
+int timerThread(int argc, char *argv[])
+{
+    startUrn(argc, argv);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    thread t1(autoSplitterThread, argc, argv);
+    thread t2(timerThread, argc, argv);
+
+    t1.join();
+    t2.join();
 
     return 0;
 }
