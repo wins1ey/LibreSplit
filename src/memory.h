426c9b4fa9030e623b7bc33d315cf4c0ca1e3732
Loomeh <loombadoop@gmail.com>
2023-07-15 19:08:29 +0100

    define process_vm_readv in memory.h

--- src/memory.h
+++ src/memory.h
@@ -2,6 +2,10 @@
 #define __MEMORY_H__
 
 #include <lua.h>
+#include <sys/uio.h>
+#include <stdlib.h>
+
+ssize_t process_vm_readv(int pid, struct iovec *mem_local, int liovcnt, struct iovec *mem_remote, int riovcnt, int flags);
 
 int read_address(lua_State* L);
 
