b56ce6743378fc09c9ab621713423754eac945a1
MxCraven <cameron@camreid.co.uk>
2024-09-24 14:00:45 +0100

    Add finished counter to splits
    * Add finished counter to splits
    
    * make format
    
    ---------
    
    Co-authored-by: Craven <cam@camreid.co.uk>

--- src/component/title.c
+++ src/component/title.c
@@ -5,6 +5,7 @@ typedef struct _LSTitle {
     GtkWidget* header;
     GtkWidget* title;
     GtkWidget* attempt_count;
+    GtkWidget* finished_count;
 } LSTitle;
 extern LSComponentOps ls_title_operations; // defined at the end of the file
 
@@ -36,6 +37,13 @@ LSComponent* ls_component_title_new()
     gtk_container_add(GTK_CONTAINER(self->header), self->attempt_count);
     gtk_widget_show(self->attempt_count);
 
+    self->finished_count = gtk_label_new(NULL);
+    add_class(self->finished_count, "finished_count");
+    gtk_widget_set_margin_start(self->finished_count, 8);
+    gtk_widget_set_valign(self->finished_count, GTK_ALIGN_START);
+    gtk_container_add(GTK_CONTAINER(self->header), self->finished_count);
+    gtk_widget_show(self->finished_count);
+
     return (LSComponent*)self;
 }
 
@@ -53,13 +61,16 @@ static void title_resize(LSComponent* self_, int win_width, int win_height)
 {
     GdkRectangle rect;
     int attempt_count_width;
+    int finished_count_width;
     int title_width;
     LSTitle* self = (LSTitle*)self_;
 
     gtk_widget_hide(self->title);
     gtk_widget_get_allocation(self->attempt_count, &rect);
     attempt_count_width = rect.width;
-    title_width = win_width - attempt_count_width;
+    gtk_widget_get_allocation(self->finished_count, &rect);
+    finished_count_width = rect.width;
+    title_width = win_width - (attempt_count_width + finished_count_width);
     rect.width = title_width;
     gtk_widget_show(self->title);
     gtk_widget_set_allocation(self->title, &rect);
@@ -77,10 +88,16 @@ static void title_show_game(LSComponent* self_, ls_game* game,
 
 static void title_draw(LSComponent* self_, ls_game* game, ls_timer* timer)
 {
-    char str[64];
+    char attempt_str[64];
+    char finished_str[64];
+    char combi_str[64];
     LSTitle* self = (LSTitle*)self_;
-    sprintf(str, "#%d", game->attempt_count);
-    gtk_label_set_text(GTK_LABEL(self->attempt_count), str);
+    sprintf(attempt_str, "%d", game->attempt_count);
+    sprintf(finished_str, "#%d", game->finished_count);
+    strcpy(combi_str, finished_str);
+    strcat(combi_str, "/");
+    strcat(combi_str, attempt_str);
+    gtk_label_set_text(GTK_LABEL(self->attempt_count), combi_str);
 }
 
 LSComponentOps ls_title_operations = {
