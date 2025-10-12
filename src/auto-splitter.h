738467c70eedd71d60cba4b6eae32dc2a334a946
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-10-11 22:34:33 -0300

    asl: Reset lua state on run cancel

--- src/auto-splitter.h
+++ src/auto-splitter.h
@@ -5,6 +5,7 @@
 #include <stdatomic.h>
 
 extern atomic_bool auto_splitter_enabled;
+extern atomic_bool auto_splitter_running;
 extern atomic_bool call_start;
 extern atomic_bool call_split;
 extern atomic_bool toggle_loading;
