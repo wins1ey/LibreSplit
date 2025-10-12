404257f969e96c8958f6db5e17e8189ba3765f7d
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-10-11 21:43:38 -0300

    asl: Implement ingame time

--- src/auto-splitter.h
+++ src/auto-splitter.h
@@ -10,6 +10,8 @@ extern atomic_bool call_start;
 extern atomic_bool call_split;
 extern atomic_bool toggle_loading;
 extern atomic_bool call_reset;
+extern atomic_bool update_game_time;
+extern atomic_llong game_time_value;
 extern char auto_splitter_file[PATH_MAX];
 extern int maps_cache_cycles_value;
 
